99c6bbc5440971b93071f8d40b062b28fbb5c2b8
Augustine A A <Augustine A A@DESKTOP-H4G81TG>
2024-07-19 15:02:15 +0100

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Todo_App.Models;
+
+namespace Todo_App.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpPost("create")]
+        public async Task<IActionResult> GetTodos([FromBody]TodoRequest request)
+        {
+             return Ok(request);
+        }
+
+    }
+}
---
+++ Models/TodoRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Todo_App.Models
+{
+    public class TodoRequest
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        [JsonIgnore]
+        public DateTime  DateCreated { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ExecutionTime { get; set; }
+        [JsonIgnore]
+        public bool Isdeleted { get; set; }
+    }
+}
